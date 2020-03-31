8a4b48ccb49086c311fd1cfb958d326ca36528cd
root <root@amber2noc.man.o3blabs.local>
2020-03-31 01:35:18 +0000

    added new changes

---
+++ conditionals.py
@@ -0,0 +1,8 @@
+likes_pineapple_on_pizza = "NO"
+
+if(likes_pineapple_on_pizza == "Absolutely!"):
+    print("You are weird!")
+elif(likes_pineapple_on_pizza == "Sort of"):
+    print("I guess you're ok")
+else:
+    print("you are completely normal")
